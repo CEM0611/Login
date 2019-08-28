dd911fdbd6d0d597f0b0cc611b2719f7240d0103
cemendoza0611 <cemendoza0611@eagle.fgcu.edu>
2019-08-28 18:44:47 -0400

    Changed name of folder "sample" to "login".

--- src/login/Main.java
+++ src/login/Main.java
@@ -1,12 +1,10 @@
-package sample;
+package login;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -14,9 +12,6 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
